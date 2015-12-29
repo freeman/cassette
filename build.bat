b559bc7d53a54c41a2d8e81d825b59d3ba4dfc1c
freeman <michel.rasschaert@gmail.com>
2015-12-29 18:46:02 +0100

    Fix build with VS2013

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-set msbuild40=%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
+set msbuild40="%ProgramFiles(x86)%\MSBuild\12.0\Bin\msbuild.exe"
 %msbuild40% /m build.3.5.xml
 %msbuild40% /m build.4.0.xml
 %msbuild40% /m build.package.xml
